0604753ec4ac567b6edee428b44caade4202b334
jonathanhand <hand.jonathan.13@gmail.com>
2019-05-07 14:02:15 -0700

    init

---
+++ src0/app/brewery-list/brewery-list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brewery-list',
+  templateUrl: './brewery-list.page.html',
+  styleUrls: ['./brewery-list.page.scss'],
+})
+export class BreweryListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src0/app/event-page/event-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-page',
+  templateUrl: './event-page.page.html',
+  styleUrls: ['./event-page.page.scss'],
+})
+export class EventPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
