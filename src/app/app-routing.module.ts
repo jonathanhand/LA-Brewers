ca93ba202c5c23f1aac5b0ee3ac8094543f8a422
jonathanhand <hand.jonathan.13@gmail.com>
2019-05-11 13:11:56 -0700

    added location page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ const routes: Routes = [
   { path: 'placedetail', loadChildren: './tab1/placedetail/placedetail.module#PlacedetailPageModule' },
   { path: 'offerdetail', loadChildren: './tab2/offerdetail/offerdetail.module#OfferdetailPageModule' },
   { path: 'newoffer', loadChildren: './tab2/newoffer/newoffer.module#NewofferPageModule' },
-  { path: 'editoffer', loadChildren: './tab2/editoffer/editoffer.module#EditofferPageModule' },  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' }
+  { path: 'editoffer', loadChildren: './tab2/editoffer/editoffer.module#EditofferPageModule' },  { path: 'tab3', loadChildren: './tab3/tab3.module#Tab3PageModule' },
+  { path: 'tab4', loadChildren: './tab4/tab4.module#Tab4PageModule' },
+  { path: 'location', loadChildren: './location/location.module#LocationPageModule' }
 
 ];
 @NgModule({
